d45dd36c9e2a82b2ac2320cae82598645c857299
MJaik <48388758+MJaik@users.noreply.github.com>
2019-03-31 12:53:59 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js')
+const client = new Discord.Client
+let prefix = '1'
+
+
+client.login('NTM0Mzc2MzE5MTA3MDcyMDAx.D3JNGg.y1-HOnQbfMCnQ8iFtG4jgK9whkI')
+
+
+
+client.on("message", async message => {   
+client.user.setActivity('наконец-то запустился',{ type: 'PLAYING' }) 
+if(message.author.bot) return;
+if(message.channel.type === "dm") return;
+
+const args = message.content.split(" ");
+
+
+switch(args[0]) {
+
+case prefix + "ping":
+ message.channel.send("секунду...").then(m => {
+
+let ping = m.createdTimestamp - message.createdTimestamp
+
+let choices = ["мой ping", "это плохо?" , "наверное это хорошо", "Pong"]
+
+let response = choices[Math.floor(Math.random() * choices.length)]
+
+
+m.edit(`${response}: ${ping}, DISCORD API ping: ${Math.round(client.ping)}`)
+
+          })
+
+             break;
+         case prefix + "help":
+                  let hembed = new Discord.RichEmbed()
+                  .setColor('#66ff66')
+                  .addField(prefix + "ping","узнать пинг бота")
+                  
+                  message.channel.send(hembed)
+                  
+                  break;
+}
+});
