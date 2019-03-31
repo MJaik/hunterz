6cc0be9dcf1e6a5591681031530f7b6c86bd7168
MJaik <48388758+MJaik@users.noreply.github.com>
2019-03-31 14:22:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ switch(args[0]) {
                   let hembed = new Discord.RichEmbed()
                   .setColor('#66ff66')
                   .addField("»»»»»Moderator«««««")
+                  .addField(prefix + "tempmute" " <@никнейм> <время> <причина>")
+                  .addField(prefix + "tempban" " <@никнейм> <время> <причина>")
                   
                   message.channel.send(hembed)
                   
