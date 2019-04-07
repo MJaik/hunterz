5df2402138173547d91f590a3bd08346d7491d6e
MJaik <48388758+MJaik@users.noreply.github.com>
2019-04-07 18:41:23 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+const bot = Discord.Client({disableEveryone = true});
+
+bot.on("ready", async () => {
+  bot.user.setActivity("Привет!", {type: "STREAMING"});
+})
+
+bot.on("message", async message => {
+  if(message.author.bot || message.channel.type = "dm") return;
+  
+  let prefix = %;
+  
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  
+  if(cmd === '%hello') {
+    return message.channel.send("Привет!");
+  }
+})
+
+bot.login(botconfig.token);
